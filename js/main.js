4ae622c454bccf3a6be4be9b9372f6bdf11fa710
Dean gaggero <deangaggero@gmail.com>
2018-02-05 20:54:50 -0800

    javascript_template

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// var testOne = "What kind of variable is this?"; // string
+// var testThree = ["What", "kind", "of", "variable", "am", "I?"]; // object
+// var testFour = false; // boolean
+// var testFive = [1, 2, 5]; //object
+// var testSeven = "What am I?" //string
+// var testEight = true; //boolean
+// var testNine = ["What kind of variable is this?", 2, 5, 19]; // object
+// var string1 = "Hello ";
+// var string2 = "my name is ";
+// var string3 = "your name here";
+// var combinedStrings = string1 + string2 + string3;
+// var num1 = 8;
+// var num2 = 52;
+// var num3 = 60;
+// var combinedNumbers = num1 + num2 + num3;
+// var mixedNum1 = 10;
+// var mixedString1 = "ten";
+// var mixedNum2 = "10";
+// var mixedConcatenation = mixedNum1 + mixedNum2 + mixedString1;
+// var num4 = 5;
+// var num5 = 0;
+// if (num4 > 1) {
+// console.log("Num4 is greater than 1");
+//} else {
+// console.log("Num4 is NOT greater than 1");
+// var num4 = 5;
+// var num5 = 0;
+// if (num4 < num5 || num4 === num5 && true) {
+// console.log("If statement ran!");
+// } else {
+// console.log("Else statement ran");
