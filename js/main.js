c557135efd538f56def125160e3aa21a426b9152
Dean gaggero <deangaggero@gmail.com>
2018-02-19 20:02:21 -0800

    update week9exercise

--- js/main.js
+++ js/main.js
@@ -1,31 +1,25 @@
-// var testOne = "What kind of variable is this?"; // string
-// var testThree = ["What", "kind", "of", "variable", "am", "I?"]; // object
-// var testFour = false; // boolean
-// var testFive = [1, 2, 5]; //object
-// var testSeven = "What am I?" //string
-// var testEight = true; //boolean
-// var testNine = ["What kind of variable is this?", 2, 5, 19]; // object
-// var string1 = "Hello ";
-// var string2 = "my name is ";
-// var string3 = "your name here";
-// var combinedStrings = string1 + string2 + string3;
-// var num1 = 8;
-// var num2 = 52;
-// var num3 = 60;
-// var combinedNumbers = num1 + num2 + num3;
-// var mixedNum1 = 10;
-// var mixedString1 = "ten";
-// var mixedNum2 = "10";
-// var mixedConcatenation = mixedNum1 + mixedNum2 + mixedString1;
-// var num4 = 5;
-// var num5 = 0;
-// if (num4 > 1) {
-// console.log("Num4 is greater than 1");
-//} else {
-// console.log("Num4 is NOT greater than 1");
-// var num4 = 5;
-// var num5 = 0;
-// if (num4 < num5 || num4 === num5 && true) {
-// console.log("If statement ran!");
-// } else {
-// console.log("Else statement ran");
+const div = document.queryselector(".one.two").style.backgroundColor = "red";
+const newDIV = document.createElement("DIV");
+newDiv.id = "temp";
+const newH1 = document.createElement("H1");
+newH1.textcontent = "sometext";
+newdiv.appendChild(newH1);
+const newDiv2 = document.createElement("DIV");
+newDiv2.id = "alsotemp";
+const newerH1 = document.createElement("H1");
+const newerH1 = document.createTextNode("some more text");
+newerH1.appendchild(textNode);
+newDiv2.appendchild(newerH1);
+
+function swapAttr() {
+  let imageTag = document.queryselector("IMG");
+  if (imagetag.alt === "lion") {
+    imageTag.alt = "tiger";
+    imageTag.src = "/img.tiger.jpg";
+  } else {
+    imageTag.alt = "lion";
+    imageTag.src = "img/lion.jpg";
+  }
+
+
+}
